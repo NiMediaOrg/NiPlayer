33fc21e1c3297ce392ce40de4fa80fb742992b46
niyuancheng <nyc20010820@126.com>
2023-01-23 23:50:14 +0800

    modified:   src/component/Controller/parts/Options.ts

--- src/component/Controller/parts/Options.ts
+++ src/component/Controller/parts/Options.ts
@@ -1,3 +1,13 @@
-export class Options {
+import { Component } from "../../../class/Component";
+import { Player } from "../../../page/player";
+import { ComponentItem, DOMProps,Node } from "../../../types/Player";
+
+export class Options extends Component implements ComponentItem{
+    id = "Options";
+    props: DOMProps;
+    constructor(player: Player, container: HTMLElement,desc?:string,props?:DOMProps,children?:Node[]) {
+        super(container, desc, props,children);
+    }
     
+
 }
