964fb857b4348e8cd294c7209c6fff0df0b3179c
niyuancheng <nyc20010820@126.com>
2023-02-08 00:18:40 +0800

    完善相关组件动画

--- src/component/Controller/parts/SubSetting.ts
+++ src/component/Controller/parts/SubSetting.ts
@@ -3,10 +3,12 @@ import { Player } from "../../../page/player";
 import { DOMProps, Node } from "../../../types/Player";
 import { storeControlComponent } from "../../../utils/store";
 import { subSettingPath } from "../path/defaultPath";
-import { $, addClass, createSvg } from "../../../utils/domUtils";
+import { $, addClass, createSvg, includeClass, removeClass } from "../../../utils/domUtils";
+import { wrap } from "ntouch.js";
 
 export class SubSetting extends Options {
     readonly id = "SubSetting";
+    clickOrTap: "click" | "singleTap"
     constructor(player: Player,container: HTMLElement, desc?: string, props?: DOMProps,children?: Node[]) {
         super(player,container,0,0,desc);
         this.init();
@@ -14,6 +16,7 @@ export class SubSetting extends Options {
 
     init() {
         this.initTemplate();
+        this.initEvent();
         storeControlComponent(this);
     }
 
@@ -25,5 +28,37 @@ export class SubSetting extends Options {
         this.iconBox.appendChild(this.icon);
         this.el.appendChild(this.iconBox);
         this.el.appendChild(this.hideBox);
+    }
+
+    initEvent(): void {
+        if(this.player.env === "PC") {
+            this.initPCEvent();
+        } else {
+            this.initMobileEvent();
+        }
+
+        this.el.onmouseenter = null;
+        wrap(this.iconBox).addEventListener(this.clickOrTap, (e) => {
+            if(!includeClass(this.icon,"video-subsettings-animate")) {
+                addClass(this.icon,["video-subsettings-animate"]);
+            } else {
+                removeClass(this.icon,["video-subsettings-animate"])
+            }
+            if(!includeClass(this.hideBox,"video-set-hidden")) {
+                addClass(this.hideBox,["video-set-hidden"]);
+            } else {
+                removeClass(this.hideBox,["video-set-hidden"]);
+            }
+            this.player.emit("oneControllerHover",this);
+        })
+
+    }
+
+    initPCEvent(): void {
+        this.clickOrTap = "click"
+    }
+
+    initMobileEvent(): void {
+        this.clickOrTap = "singleTap"
     }
 }
