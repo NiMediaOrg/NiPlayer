2eeaf752cda990f623e587248ad8cd7d583e4c15
niyuancheng <nyc20010820@126.com>
2023-02-11 22:29:15 +0800

    tiny change

--- src/component/ToolBar/MediumBar/MediumBar.ts
+++ src/component/ToolBar/MediumBar/MediumBar.ts
@@ -1,15 +1,16 @@
 import { Component } from "../../../class/Component";
 import { Player } from "../../../page/player";
 import { ComponentItem } from "../../../types/Player";
-import { $, addClass } from "../../../utils/domUtils";
+import { $ } from "../../../utils/domUtils";
 import { storeControlComponent } from "../../../utils/store";
+import { VideoProgress } from "./parts/VideoProgressBar";
 
 export class MediumBar extends Component implements ComponentItem {
     readonly id = "MediumBar";
     leftArea: HTMLElement;
     mediumArea: HTMLElement;
     rightArea: HTMLElement;
-
+    videoProgress: VideoProgress;
     player: Player;
     
     constructor(player: Player, container: HTMLElement, desc? :string) {
@@ -20,6 +21,7 @@ export class MediumBar extends Component implements ComponentItem {
 
     init(): void {
         this.initTemplate();
+        this.initComponent()
         storeControlComponent(this);
     }
 
@@ -28,5 +30,10 @@ export class MediumBar extends Component implements ComponentItem {
         this.mediumArea = $("div.video-mediumbar-medium");
         this.rightArea = $("div.video-mediumbar-right");
         this.el.append(this.leftArea,this.mediumArea,this.rightArea);
+    }
+
+    initComponent(): void {
+        this.videoProgress = new VideoProgress(this.player,this.el,"div");
+        this.mediumArea.append(this.videoProgress.el)
     }
 }
