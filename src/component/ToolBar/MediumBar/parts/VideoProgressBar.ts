9684d9c90e817d121349261705543df5fa766e48
niyuancheng <nyc20010820@126.com>
2023-02-22 16:51:34 +0800

    pretime预览

--- src/component/ToolBar/MediumBar/parts/VideoProgressBar.ts
+++ src/component/ToolBar/MediumBar/parts/VideoProgressBar.ts
@@ -1,79 +1,109 @@
 import { EVENT } from "../../../../events";
 import { Player } from "../../../../page/player";
-import { addClass, removeClass } from "../../../../utils/domUtils";
+import { formatTime } from "../../../../utils";
+import { $, addClass, removeClass } from "../../../../utils/domUtils";
 import { Progress } from "../../../Progress/Progress";
 
 export class VideoProgress extends Progress {
-    readonly id = "VideoProgress";
-    constructor(player:Player,container?:HTMLElement,desc?:string) {
-        super(player,container,desc);
+  readonly id = "VideoProgress";
+  private preTime: HTMLElement;
+  constructor(player: Player, container?: HTMLElement, desc?: string) {
+    super(player, container, desc);
 
-        this.init()
-    }
+    this.init();
+  }
 
-    init(): void {
-        this.initTemplate()
-        this.initEvent();
-    }
+  init(): void {
 
-    initTemplate(): void {
-        addClass(this.el,["video-progress"])
-        addClass(this.dot,["video-progress-dot","video-progress-dot-hidden"])
-        addClass(this.completedProgress,["video-progress-completed"])
-    }
+    this.initTemplate();
+    this.initEvent();
+  }
 
-    initEvent(): void {
-        this.on(EVENT.PROGRESS_CLICK,(dx: number, ctx: Progress) => {
-            let scale = dx / this.el.clientWidth;
-            if (scale < 0) {
-                scale = 0;
-            } else if (scale > 1) {
-                scale = 1;
-            }
-            this.player.video.currentTime = scale * this.player.video.duration;
+  initTemplate(): void {
+    this.preTime = $("div.video-progress-pretime");
+    this.el.append(this.preTime)
+    addClass(this.el, ["video-progress"]);
+    addClass(this.dot, ["video-progress-dot", "video-progress-dot-hidden"]);
+    addClass(this.completedProgress, ["video-progress-completed"]);
+  }
 
-            if(this.player.video.paused) {
-                this.player.video.play();
-            }
-        })
+  initEvent(): void {
+    this.el.addEventListener("mouseenter",(e: MouseEvent) => {
+        this.preTime.style.display = "block";
 
-        this.on(EVENT.DOT_DRAG,(dx: number, ctx: Progress) => {
-            let scale = (dx + this.dotLeft) / this.el.clientWidth;
-            if (scale < 0) {
-                scale = 0;
-            } else if (scale > 1) {
-                scale = 1;
-            }
+        this.preTime.style.left = e.offsetX - this.preTime.clientWidth / 2 + "px";
+        let time = formatTime(
+            (e.offsetX / (e.currentTarget as HTMLElement).clientWidth) *
+              this.player.video.duration
+        );
 
-            this.player.emit(EVENT.VIDEO_DOT_DRAG, scale);
-        })
+        this.preTime.innerText = time;
+    })
 
-        this.on(EVENT.PROGRESS_MOUSE_ENTER,() => {
-            removeClass(this.dot,["video-progress-dot-hidden"])
-        })
+    this.el.addEventListener("mousemove", (e: MouseEvent) => {
+      let time = formatTime(
+        (e.offsetX / (e.currentTarget as HTMLElement).clientWidth) *
+          this.player.video.duration
+      );
 
-        this.on(EVENT.PROGRESS_MOUSE_LEAVE,() => {
-            addClass(this.dot,["video-progress-dot-hidden"])
-        })
+      this.preTime.style.left = e.offsetX - this.preTime.clientWidth / 2 + "px";
+      this.preTime.innerText = time;
+      console.log(time);
+    });
 
-        this.on(EVENT.DOT_DOWN,() => {
-            this.player.emit(EVENT.DOT_DOWN)
-        }) 
+    this.el.addEventListener("mouseleave",() => {
+        this.preTime.style.display = ""
+    })
 
-        this.on(EVENT.DOT_UP,(scale: number) => {
-            this.player.emit(EVENT.DOT_UP)
+    this.on(EVENT.PROGRESS_CLICK, (dx: number, ctx: Progress) => {
+      let scale = dx / this.el.clientWidth;
+      if (scale < 0) {
+        scale = 0;
+      } else if (scale > 1) {
+        scale = 1;
+      }
+      this.player.video.currentTime = scale * this.player.video.duration;
 
-            this.player.video.currentTime = scale * this.player.video.duration;
-        })
+      if (this.player.video.paused) {
+        this.player.video.play();
+      }
+    });
 
-        this.player.video.addEventListener("timeupdate",(e) => {
-            if(this.player.enableSeek) {
-                let scale = this.player.video.currentTime / this.player.video.duration;
-                this.dot.style.left = scale * 100 + "%";
-                this.completedProgress.style.width = scale * 100 + "%";
-            }
-            
-        })
-    }
+    this.on(EVENT.DOT_DRAG, (dx: number, ctx: Progress) => {
+      let scale = (dx + this.dotLeft) / this.el.clientWidth;
+      if (scale < 0) {
+        scale = 0;
+      } else if (scale > 1) {
+        scale = 1;
+      }
 
+      this.player.emit(EVENT.VIDEO_DOT_DRAG, scale);
+    });
+
+    this.on(EVENT.PROGRESS_MOUSE_ENTER, () => {
+      removeClass(this.dot, ["video-progress-dot-hidden"]);
+    });
+
+    this.on(EVENT.PROGRESS_MOUSE_LEAVE, () => {
+      addClass(this.dot, ["video-progress-dot-hidden"]);
+    });
+
+    this.on(EVENT.DOT_DOWN, () => {
+      this.player.emit(EVENT.DOT_DOWN);
+    });
+
+    this.on(EVENT.DOT_UP, (scale: number) => {
+      this.player.emit(EVENT.DOT_UP);
+
+      this.player.video.currentTime = scale * this.player.video.duration;
+    });
+
+    this.player.video.addEventListener("timeupdate", (e) => {
+      if (this.player.enableSeek) {
+        let scale = this.player.video.currentTime / this.player.video.duration;
+        this.dot.style.left = scale * 100 + "%";
+        this.completedProgress.style.width = scale * 100 + "%";
+      }
+    });
+  }
 }
