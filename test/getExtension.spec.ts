9ff3b08fd1a1c8eac0a81074150360c0bb6e55f5
niyuancheng <nyc20010820@126.com>
2023-06-26 13:16:46 +0800

    update: 优化导入路径

--- test/getExtension.spec.ts
+++ test/getExtension.spec.ts
@@ -1,5 +1,5 @@
 import {test,expect} from "vitest"
-import { getExtension } from "../src/utils"
+import { getExtension } from "../src"
 test("getExtension", () => {
     expect(getExtension('https://jsdeliver.cdn/446-bps-media.mpd')).toBe("mpd");
     expect(getExtension("https://baidu")).toBe("https://baidu");
