2f5bfb0328d4985301884f83d254dd44be5ef9d4
niyuancheng <niyuancheng@bilibili.com>
2024-08-12 20:25:15 +0800

    feat: add setting animate

--- packages/niplayer/src/plugin/setting/index.tsx
+++ packages/niplayer/src/plugin/setting/index.tsx
@@ -3,7 +3,6 @@ import { UIPlugin } from "@/base/ui.plugin";
 import { createSignal, JSX } from "solid-js";
 import { IPanelItem, Panel } from "niplayer-components";
 import { playrate, ratio, subtitle, rightArrow } from "@/assets/svg";
- 
 import "./index.less";
 
 export class Setting extends UIPlugin {
@@ -13,13 +12,39 @@ export class Setting extends UIPlugin {
             content: '画面比例',
             icon: ratio,
             tip: '正常',
-            button: rightArrow
+            button: rightArrow,
         },
         {
             content: '倍速',
             icon: playrate,
             tip: '正常',
             button: rightArrow,
+            jump: [
+                {
+                    content: '0.25'
+                },
+                {
+                    content: '0.5'
+                },
+                {
+                    content: '0.75'
+                },
+                {
+                    content: '正常'
+                },
+                {
+                    content: '1.25'
+                },
+                {
+                    content: '1.5'
+                },
+                {
+                    content: '1.75'
+                },
+                {
+                    content: '2'
+                }
+            ]
         },
         {
             content: '字幕',
@@ -31,15 +56,21 @@ export class Setting extends UIPlugin {
 
     protected render(): JSX.Element | string | HTMLElement {
         const [ settingPanelShow, setSettingPanelShow ] = createSignal(false);
+        const [ sideItems, setSideItems ] = createSignal([]);
         const handleClick = () => {
             setSettingPanelShow(v => !v);
         }
 
+        const handleItemClick = (list: IPanelItem[]) => {
+            // console.log(list)
+            setSideItems(list);
+        }
+
         return (
-            <div class="niplayer-controller-middle-item niplayer-setting-controller" onClick={handleClick}>
-                <div innerHTML={setting} style={{width: '48px', height: '48px'}}></div>
+            <div class="niplayer-controller-middle-item niplayer-setting-controller">
+                <div innerHTML={setting} style={{width: '48px', height: '48px'}} onClick={handleClick}></div>
                 <div class="niplayer-setting-panel-container">
-                    <Panel items={this.panelItems} />
+                    <Panel items={this.panelItems} hidden={!settingPanelShow()} onItemClick={handleItemClick} sideItems={sideItems()}/>
                 </div>
             </div>
         )
