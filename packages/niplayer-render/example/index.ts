d69c23622f225ede2aaa674f68ad852ae2ebbdbd
niyuancheng <niyuancheng@bilibili.com>
2024-08-23 17:22:54 +0800

    feat: 完善绘制能力

--- packages/niplayer-render/example/index.ts
+++ packages/niplayer-render/example/index.ts
@@ -12,14 +12,19 @@ const app = new Application({
 const rect1 = new Rectangle(10, 10, 400, 400);
 rect1.style.color ='red';
 rect1.style.zIndex = 20;
+rect1.style.position = 'absolute';
+rect1.style.font = '35px Arial';
+rect1.style.overflow = 'hidden';
+rect1.innerText = 'Hellooooooooooooooooooooo World';
 
 const rect2 = new Rectangle(40, 30, 50, 200);
 rect2.style.color = 'blue';
 rect2.style.zIndex = 10;
+rect2.style.position = 'absolute';
 
 const rect3 = new Rectangle(50, 50, 160, 100)
 rect3.style.color = 'green';
-rect3.style.zIndex = 30
+rect3.style.zIndex = 30;
 
 rect1.appendChild(rect2);
 rect1.appendChild(rect3);
@@ -28,7 +33,6 @@ app.appendChild(rect1);
 app.draw();
 
 window.setTimeout(() => {
-    rect1.style.left = 200;
-    rect1.style.top = 200;
-    rect1.style.color = 'blue';
+    rect1.style.x = 200;
+    rect1.style.y = 200;
 }, 2000)
--- packages/niplayer-render/src/material/Rectangle.ts
+++ packages/niplayer-render/src/material/Rectangle.ts
@@ -6,27 +6,56 @@ export class Rectangle extends Graphics {
         return true;
     }
 
+    public innerText: string = '';
+
     constructor(
-        x: number, 
+        x: number,
         y: number,
-        width: number, 
+        width: number,
         height: number
     ) {
         super();
         this.style.height = height;
         this.style.width = width;
-        this.style.left = x;
-        this.style.top = y;
+        this.style.x = x;
+        this.style.y = y;
     }
 
     public draw(context: CanvasRenderingContext2D): void {
         context.save();
         context.beginPath();
+
         this.style.color && (context.fillStyle = this.style.color);
         this.style.opacity && (context.globalAlpha = this.style.opacity);
-        context.fillRect(this.style.left, this.style.top, this.style.width, this.style.height);
+        const node = this.findNearPositionNode(this.style.position);
+        const x = this.style.x + (node?.style?.x || 0);
+        const y = this.style.y + (node?.style?.y || 0);
+        context.fillRect(x, y, this.style.width, this.style.height);
         context.closePath();
         context.restore();
+        this.drawText(context);
         this.children.forEach(child => child.draw(context));
+    }
+
+    drawText(context: CanvasRenderingContext2D) {
+        if (!this.innerText) {
+            return;
+        }
+        context.save();
+        context.beginPath();
+        context.textAlign = 'left';
+        context.textBaseline = 'top';
+        context.font = this.style.font ?? '25px Arial';
+        context.fillStyle = 'black';
+        context.fillText(this.innerText, this.style.x, this.style.y + 10);
+        context.fill();
+        const { actualBoundingBoxAscent, actualBoundingBoxDescent, actualBoundingBoxLeft, actualBoundingBoxRight } = context.measureText(this.innerText);
+        const height = actualBoundingBoxAscent + actualBoundingBoxDescent;
+        const width = actualBoundingBoxLeft + actualBoundingBoxRight;
+        if (width > this.style.width && this.style.overflow === 'hidden') {
+            context.clearRect(this.style.x + this.style.width, this.style.y + 10, width - this.style.width + 5, height + 5);
+        }
+        context.closePath();
+        context.restore();
     }
 }
